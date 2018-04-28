4f89bb678cd355ee88625ce98d663127187be49e
Jackson <35349831+jtagt@users.noreply.github.com>
2018-04-28 14:30:43 -0700

    added basic framework gonna start adding dirs

--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
+import asyncio
+import logging
+import os
+from datetime import datetime
 
+class Bot(commands.AutoShardedBot):
+
+    @staticmethod
+    def prefix_from(bot, msg):
+        # must be an instance of this bot pls dont use anything else
+        if not msg.guild:
+            return bot.bot_settings.prefix
+        return bot.prefix_map.get(msg.guild.id, bot.bot_settings.prefix)
+
+    def __init__(self, bot_settings, **kwargs):
+        super().__init__(Bot.prefix_from, **kwargs)
+        self.logger = logging.getLogger("bot")
+        self.start_time = datetime.now()
+        self.bot_settings = bot_settings
+        self.prefix_map = {}
+        self.mpm = None
+        self.ready = False
+        
+        """Start here"""
